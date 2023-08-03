e87827f9f1bc69024b2851d76b903933da8aec38
Eidi Yuki <eidiyuki21@gmail.com>
2023-08-03 12:52:14 -0300

    Foram implementados os algoritmos dos botões 'subtrair', 'dividir', 'multiplicar', 'igual' e 'ponto'.

--- script.js
+++ script.js
@@ -11,20 +11,69 @@ let plus = document.querySelector(".plus");
 let minus = document.querySelector(".minus");
 let array = [];
 let newArray = [];
+let operators = [];
 let total;
+
+let calc = (operator) => {
+  switch (operator) {
+    case "+":
+      total += newArray[newArray.length - 1];
+      break;
+    case "-":
+      total -= newArray[newArray.length - 1];
+      break;
+    case "*":
+      total *= newArray[newArray.length - 1];
+      break;
+    case "÷":
+      total /= newArray[newArray.length - 1];
+      break;
+  }
+};
+
+let update = () => {
+  calc(operators[operators.length - 1]);
+  bottomScreen.innerText = total;
+};
+
+let showScreenTop = (operator) => {
+  if (Number.isInteger(total) == true) {
+    topScreen.innerText = `${total} ${operator}`;
+  } else {
+    topScreen.innerText = `${total.toFixed(2)} ${operator} `;
+  }
+};
+
+let showScreenBottom = () => {
+  if (Number.isInteger(total) == true) {
+    bottomScreen.innerText = total;
+  } else {
+    bottomScreen.innerText = total.toFixed(2);
+  }
+};
+
+let errorMessage = () => {
+  if (total == Infinity || total == -Infinity) {
+    topScreen.innerText = `ERROR!`;
+    bottomScreen.innerText = `PRESS CLEAR TO CONTINUE`;
+  }
+};
 
 button.forEach((element) =>
   element.addEventListener("click", () => {
     array.push(element.innerText);
     bottomScreen.innerText = array.join("");
+    errorMessage();
   })
 );
 
 dot.addEventListener("click", () => {
-  if (bottomScreen.innerText.includes(dot.innerText)) {
+  if (bottomScreen.innerText.length == 0) {
+  } else if (bottomScreen.innerText.includes(dot.innerText)) {
   } else {
     array.push(dot.innerText);
     bottomScreen.innerText = array.join("");
+    errorMessage();
   }
 });
 
@@ -33,57 +82,108 @@ clear.addEventListener("click", () => {
   bottomScreen.innerText = "";
   array = [];
   newArray = [];
-  console.log([array]);
-  console.log([newArray]);
+  operators = [];
+  total = 0;
 });
 
 del.addEventListener("click", () => {
   array.pop("");
   bottomScreen.innerText = array.join("");
+  errorMessage();
 });
 
 plus.addEventListener("click", () => {
   if (array.length == 0) {
-  } else {
-    topScreen.innerText += ` ${array.join("")} +  `;
+  } else if (newArray.length > 0) {
     newArray.push(Number(array.join("")));
-
+    update();
+    operators.push("+");
+    showScreenTop("+");
     array = [];
+
+    showScreenBottom();
+  } else {
+    newArray.push(Number(array.join("")));
+    total = newArray[0];
+    showScreenTop("+");
+    array = [];
+    operators.push("+");
   }
-  total = newArray.reduce((sum, element) => sum + element, 0);
-  console.log(total);
-  bottomScreen.innerText = total;
+  errorMessage();
 });
 
 minus.addEventListener("click", () => {
   if (array.length == 0) {
-  } else {
-    array.push(" - ");
-    console.table(array);
-    topScreen.innerText += ` ${array.join("")} `;
-    newArray.push(array.join(""));
+    array.push("-");
+    bottomScreen.innerHTML = array.join("");
+  } else if (newArray.length > 0) {
+    newArray.push(Number(array.join("")));
+    update();
+    topScreen.innerText = `${total} -`;
     array = [];
+    operators.push("-");
+    showScreenTop("-");
+    showScreenBottom();
+  } else {
+    newArray.push(Number(array.join("")));
+    total = newArray[0];
+    showScreenTop("-");
+    array = [];
+    operators.push("-");
   }
+  errorMessage();
 });
 
 multiply.addEventListener("click", () => {
   if (array.length == 0) {
-  } else {
-    array.push(" * ");
-    console.table(array);
-    topScreen.innerText += ` ${array.join("")} `;
-    newArray.push(array.join(""));
+  } else if (newArray.length > 0) {
+    newArray.push(Number(array.join("")));
+    update();
+    topScreen.innerText = `${total} *`;
     array = [];
+    operators.push("*");
+    showScreenTop("*");
+    showScreenBottom();
+  } else {
+    newArray.push(Number(array.join("")));
+    total = newArray[0];
+    showScreenTop("*");
+    array = [];
+    operators.push("*");
   }
+  errorMessage();
 });
 
 division.addEventListener("click", () => {
   if (array.length == 0) {
-  } else {
-    array.push(" / ");
-    console.table(array);
-    topScreen.innerText += ` ${array.join("")} `;
-    newArray.push(array.join(""));
+  } else if (newArray.length > 0) {
+    newArray.push(Number(array.join("")));
+    update();
+    topScreen.innerText = `${total} ÷`;
     array = [];
+    operators.push("÷");
+    showScreenTop("÷");
+    showScreenBottom();
+  } else {
+    newArray.push(Number(array.join("")));
+    total = newArray[0];
+    showScreenTop("÷");
+    array = [];
+    operators.push("÷");
   }
+  errorMessage();
+});
+
+equal.addEventListener("click", () => {
+  if (array.length == 0) {
+  } else if (newArray.length > 0) {
+    topScreen.innerText += ` ${array.join("")} =  `;
+    newArray.push(Number(array.join("")));
+    calc(operators[operators.length - 1]);
+
+    showScreenBottom();
+    array = [total];
+    newArray = [];
+  }
+  errorMessage();
 });
